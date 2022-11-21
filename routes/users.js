5f1f40d00468ea2a755bf91ea09100319a22c670
kevinsherrell <k.sherrell.1028@gmail.com>
2022-11-20 19:27:04 -0600

    complete

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,34 @@ const saltRounds = 10;
 const {db} = require('../mongo');
 const {uuid} = require('uuidv4');
 const {validateUser} = require('../validation/user');
+const jwt = require('jsonwebtoken');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
-
+router.get('/message', (req, res)=>{
+    const token = req.header(process.env.TOKEN_HEADER_KEY);
+    console.log(token);
+    const verified = jwt.verify(token,process.env.JWT_SECRET_KEY);
+    if(verified === null){
+        res.json({
+            success: false,
+            message: 'ID token could not be verified'
+        })
+    }
+    if(verified && verified.userData.scope === 'user'){
+        res.json({
+            success: true,
+            message: "I am a user"
+        });
+    }else if(verified && verified.userData.scope === 'admin'){
+        res.json({
+            success: true,
+            message: "I am an admin"
+        })
+    }
+    console.log(verified);
+})
 /* POST create user*/
 router.post('/create', async (req, res) => {
     console.log("create has been called");
@@ -67,13 +90,30 @@ router.post('/create', async (req, res) => {
 
 /* POST login user*/
 router.post('/login', async (req, res) => {
+
+    const secretKey = process.env.JWT_SECRET_KEY;
     console.log("loggin running");
     const foundUser = await db().collection('users').findOne({email: req.body.email});
+    const userData = {
+        date: new Date(),
+        id: foundUser._id,
+        scope: foundUser.email.includes("codeimmersives.com") ? 'admin' : 'user'
+    }
+    const payload = {
+        userData: userData,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60)
+    };
+
     bcrypt.compare(req.body.password, foundUser.password)
         .then(result => {
             if (result === true) {
+                const token = jwt.sign(payload, secretKey);
+
+
                 res.json({
-                    success: true
+                    success: true,
+                    token: token,
+                    email: foundUser.email
                 })
             } else {
                 res.json({
